d9afee7a4b48d5a16bebe409b93818422a554014
kramskoy <kramskoy.va@gmail.com>
2023-11-18 00:00:24 +0300

    Добавлен список мотоциклов, правка услуг

--- src/js/components/classes.js
+++ src/js/components/classes.js
@@ -4,8 +4,8 @@ export class Lists {
     this.text = text;
     this.tag = tag;
     this.elemInsert = elemInsert;
-    this.anyValue = anyValue
-    this.anyText = anyText
+    this.anyValue = anyValue;
+    this.anyText = anyText;
   }
 
   renderCategoryTitles() {
@@ -18,43 +18,27 @@ export class Lists {
     const element = document.createElement(`${this.tag}`);
     element.classList.add(`${this.anyValue}`);
     element.innerHTML = `
-       <p>${this.text}</p>
-       <b>${this.anyText} ₽</b>
-      `;
+			 <p>${this.text}</p>
+			 <b>${this.anyText}</b>
+			`;
     this.elemInsert.append(element);
   }
   renderBrands() {
     const element = document.createElement(`${this.tag}`);
-    element.setAttribute('value', `${this.anyValue}-${this.text}`);
+    element.setAttribute("value", `${this.anyValue}-${this.text}`);
     element.innerText = this.text;
     this.elemInsert.before(element);
   }
   renderYears() {
     const element = document.createElement(`${this.tag}`);
-    element.setAttribute('value', `${this.text}`);
+    element.setAttribute("value", `${this.text}`);
     element.innerText = this.text;
     this.elemInsert.before(element);
   }
   renderModel() {
     const element = document.createElement(`${this.tag}`);
-    element.setAttribute('value', `${this.text}`);
+    element.setAttribute("value", `${this.text}`);
     element.innerText = this.text;
     this.elemInsert.append(element);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
