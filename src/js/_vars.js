2aa82cccf98b2dcf8742d87df71b4b1670d57b69
kramskoy-v <kramskoy.va@gmail.com>
2023-11-20 20:42:21 +0300

    Формат кода

--- src/js/_vars.js
+++ src/js/_vars.js
@@ -1,17 +1,19 @@
-export const otherBrands = document.querySelectorAll('.other-brand')
+export const otherBrands = document.querySelectorAll(".other-brand");
 export const brandSelects = document.querySelectorAll('select[name="brand"]');
 export const modelSelects = document.querySelectorAll('select[name="model"]');
 export const engineSelects = document.querySelectorAll('select[name="engine"]');
-export const yearsSelector = document.querySelectorAll('.other-years')
-export const bidBtns = document.querySelectorAll('.btn.bid');
-export const phoneBtn = document.querySelector('.header__phone.btn');
-export const adaptiveMenu = document.querySelector('.header__adaptive-wrapper');
-export const headerContainer = document.querySelector('.header .container');
-export const servicesListTitles = document.querySelector('.services__list-titles');
-export const servicesList = document.querySelector('.services__list');
-export const subtitle = document.querySelector('.services__subtitle');
-export const categoryBtn = document.querySelector('.services__category-btn');
-export const modal = document.querySelector('.modal');
-export const modalOk = document.querySelector('.modal-ok');
+export const yearsSelector = document.querySelectorAll(".other-years");
+export const bidBtns = document.querySelectorAll(".btn.bid");
+export const phoneBtn = document.querySelector(".header__phone.btn");
+export const adaptiveMenu = document.querySelector(".header__adaptive-wrapper");
+export const headerContainer = document.querySelector(".header .container");
+export const servicesListTitles = document.querySelector(
+	".services__list-titles"
+);
+export const servicesList = document.querySelector(".services__list");
+export const subtitle = document.querySelector(".services__subtitle");
+export const categoryBtn = document.querySelector(".services__category-btn");
+export const modal = document.querySelector(".modal");
+export const modalOk = document.querySelector(".modal-ok");
 export const screenWidth = window.innerWidth;
 export const curYear = new Date().getFullYear();
--- src/js/components/classes.js
+++ src/js/components/classes.js
@@ -1,44 +1,44 @@
-// список названия категорий
+// Рендер списка в селектах
 export class Lists {
-  constructor(text, tag, elemInsert, anyValue, anyText) {
-    this.text = text;
-    this.tag = tag;
-    this.elemInsert = elemInsert;
-    this.anyValue = anyValue;
-    this.anyText = anyText;
-  }
+	constructor(text, tag, elemInsert, anyValue, anyText) {
+		this.text = text;
+		this.tag = tag;
+		this.elemInsert = elemInsert;
+		this.anyValue = anyValue;
+		this.anyText = anyText;
+	}
 
-  renderCategoryTitles() {
-    const element = document.createElement(`${this.tag}`);
-    element.classList.add(`${this.anyValue}`);
-    element.innerText = this.text;
-    this.elemInsert.append(element);
-  }
-  renderCategory() {
-    const element = document.createElement(`${this.tag}`);
-    element.classList.add(`${this.anyValue}`);
-    element.innerHTML = `
+	renderCategoryTitles() {
+		const element = document.createElement(`${this.tag}`);
+		element.classList.add(`${this.anyValue}`);
+		element.innerText = this.text;
+		this.elemInsert.append(element);
+	}
+	renderCategory() {
+		const element = document.createElement(`${this.tag}`);
+		element.classList.add(`${this.anyValue}`);
+		element.innerHTML = `
 			 <p>${this.text}</p>
 			 <b>${this.anyText}</b>
 			`;
-    this.elemInsert.append(element);
-  }
-  renderBrands() {
-    const element = document.createElement(`${this.tag}`);
-    element.setAttribute("value", `${this.anyValue}-${this.text}`);
-    element.innerText = this.text;
-    this.elemInsert.before(element);
-  }
-  renderYears() {
-    const element = document.createElement(`${this.tag}`);
-    element.setAttribute("value", `${this.text}`);
-    element.innerText = this.text;
-    this.elemInsert.before(element);
-  }
-  renderModel() {
-    const element = document.createElement(`${this.tag}`);
-    element.setAttribute("value", `${this.text}`);
-    element.innerText = this.text;
-    this.elemInsert.append(element);
-  }
+		this.elemInsert.append(element);
+	}
+	renderBrands() {
+		const element = document.createElement(`${this.tag}`);
+		element.setAttribute("value", `${this.anyValue}-${this.text}`);
+		element.innerText = this.text;
+		this.elemInsert.before(element);
+	}
+	renderYears() {
+		const element = document.createElement(`${this.tag}`);
+		element.setAttribute("value", `${this.text}`);
+		element.innerText = this.text;
+		this.elemInsert.before(element);
+	}
+	renderModel() {
+		const element = document.createElement(`${this.tag}`);
+		element.setAttribute("value", `${this.text}`);
+		element.innerText = this.text;
+		this.elemInsert.append(element);
+	}
 }
